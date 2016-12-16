8eec48574bd9dc0c04680cc9a50894bd3117c64f
pbraunstein <pbraunstein12@gmail.com>
2016-12-15 21:55:28 -0500

    Create config file

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+from random import choice
+
+home_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY_CHAR_SET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    SECRET_KEY_LENGTH = 60
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = ''.join([choice(SECRET_KEY_CHAR_SET) for x in range(SECRET_KEY_LENGTH)])  # randomly generated
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = False
+
+class LocalConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
