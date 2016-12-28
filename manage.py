b244a163209e68363bad9f47c22d9f19e38e5bf6
pbraunstein <pbraunstein12@gmail.com>
2016-12-28 15:04:11 -0700

    Individual import commands still not implemented

--- manage.py
+++ manage.py
@@ -20,5 +20,15 @@ def run_importers():
     """
     print 'Hello World!'
 
+
+@manager.command
+def import_rep_taxonomies():
+    raise NotImplementedError("import_rep_taxonomies is not yet implemented")
+
+
+@manager.command
+def import_rep_history():
+    raise NotImplementedError("import_rep_history is not yet implemented")
+
 if __name__ == '__main__':
     manager.run()
