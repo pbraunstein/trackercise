fc7d3ef602afddc40deff4d763d35c650ad1d68a
pbraunstein <pbraunstein12@gmail.com>
2016-12-29 18:04:18 -0700

    Display sample data from rep exercise history on main page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,9 +10,19 @@ db = SQLAlchemy(app)
 
 from models import RepExercisesHistory, RepExercisesTaxonomy
 
+
 @app.route('/')
 def hello():
-    return "Hello World!"
+    results = db.engine.execute('select * from rep_exercises_history;')
+    entries = list(results)
+    entries = [_prepare_entry(x) for x in entries]
+    return render_template('index.html', entries=entries)
+
+
+def _prepare_entry(entry):
+    values = entry.values()
+    values[-1] = str(values[-1])
+    return values
 
 
 if __name__ == '__main__':
