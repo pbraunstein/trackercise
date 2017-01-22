4b859ae0098bfe21912031c3aeac64681bde1f5b
pbraunstein <pbraunstein12@gmail.com>
2017-01-21 20:06:42 -0500

    Migration finalized

--- manage.py
+++ manage.py
@@ -31,7 +31,7 @@ def import_users():
     hasher = hashlib.sha256()
     hasher.update('password')
     password = hasher.hexdigest()
-    sample_user = Users('Phil', 'this-is-a-fake-email@phil.phil', password)
+    sample_user = Users('pbraunstein12@gmail.com', 'Phil', password)
     db.session.add(sample_user)
     db.session.commit()
 
@@ -74,13 +74,13 @@ def import_rep_history():
     Imports the rep exercise history sample data into the rep_exercises_history db table
     """
     entries = []
-    user_id = Users.query.first().id
+    user_email = Users.query.first().email
     with open(os.path.join(app.root_path, 'sample_data/rep_history.csv'), 'rb') as csvfile:
         history_reader = reader(csvfile)
         history_reader.next()  # skip header line
         for row in history_reader:
             try:
-                entries.append(_generate_rep_history_from_row(row, user_id))
+                entries.append(_generate_rep_history_from_row(row, user_email))
             except ValueError:
                 pass
     db.session.add_all(entries)
@@ -103,20 +103,20 @@ def _booleanize(yes_or_no):
         raise ValueError("{0} is not a yes or no".format(yes_or_no))
 
 
-def _generate_rep_history_from_row(row, user_id):
+def _generate_rep_history_from_row(row, user_email):
     """
     Generates a RepExercisesHistory object from one row of the csv file
 
     !!! Raises ValueError
     :param row: from csv sample data for rep exercises history csv file
-    :param user_id: id of user
+    :param user_email: email of user
     :return: RepExercisesHistory taxonomy object representing one row in the db table
     """
     # if weight is body weight, signal that with -1
     if row[3] == 'body':
         row[3] = '-1'
     return RepExercisesHistory(
-        user_id=user_id,
+        user_email=user_email,
         exercise_id=_get_exercise_id_for_name(row[0]),
         sets=int(row[1]),
         reps=int(row[2]),
