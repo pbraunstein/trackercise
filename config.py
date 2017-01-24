2faf17317cfca43b536b3e176d83f1222b0a0e8a
pbraunstein <pbraunstein12@gmail.com>
2017-01-23 20:43:58 -0500

    YOLO push get secret key from environment

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
-from random import choice
 
 home_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY_CHAR_SET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    SECRET_KEY_LENGTH = 60
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = 'this_is_secret'
+    SECRET_KEY = os.environ['TRACKERCISE_SECRET_KEY']
 
 
 class ProductionConfig(Config):
