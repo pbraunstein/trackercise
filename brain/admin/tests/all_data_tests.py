c08f1c5e3d3397819fbed13cc60d1c71610ad8dd
pbraunstein <pbraunstein12@gmail.com>
2017-01-26 20:35:06 -0500

    Unit tests for AllData

--- brain/admin/tests/all_data_tests.py
+++ brain/admin/tests/all_data_tests.py
@@ -3,7 +3,7 @@ import unittest
 
 import app
 from brain.admin.all_data import AllData
-from models import RepExercisesHistory
+from models import RepExercisesHistory, RepExercisesTaxonomy
 
 
 class AllDataTests(unittest.TestCase):
@@ -18,3 +18,33 @@ class AllDataTests(unittest.TestCase):
         )
         test_entry = AllData._prepare_history_entry(test_entry)
         self.assertIsInstance(test_entry.date, str)
+
+    def test_prepare_taxonomy_entry_debooleanizes(self):
+        exercise_name = 'test_awesomeness_exercise'
+        test_entry = RepExercisesTaxonomy(
+            name=exercise_name,
+            is_back=True,
+            is_chest=False,
+            is_shoulders=True,
+            is_biceps=True,
+            is_triceps=False,
+            is_legs=False,
+            is_core=True,
+            is_balance=True,
+            is_cardio=False,
+            is_weight_per_hand=False
+        )
+
+        new_entry = AllData._prepare_taxonomy_entry(test_entry)
+
+        self.assertEqual(new_entry.name, exercise_name)
+        self.assertEqual(new_entry.is_back, 'YES')
+        self.assertEqual(new_entry.is_chest, 'NO')
+        self.assertEqual(new_entry.is_shoulders, 'YES')
+        self.assertEqual(new_entry.is_biceps, 'YES')
+        self.assertEqual(new_entry.is_triceps, 'NO')
+        self.assertEqual(new_entry.is_legs, 'NO')
+        self.assertEqual(new_entry.is_core, 'YES')
+        self.assertEqual(new_entry.is_balance, 'YES')
+        self.assertEqual(new_entry.is_cardio, 'NO')
+        self.assertEqual(new_entry.is_weight_per_hand, 'NO')
