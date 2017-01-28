ce36b3e8c5a10db78e5766c8106d84096efcf4e0
pbraunstein <pbraunstein12@gmail.com>
2017-01-28 18:37:18 -0500

    Second draft of script, draft 1

--- dev_tools/class_interface_contract_generator.py
+++ dev_tools/class_interface_contract_generator.py
@@ -20,40 +20,42 @@ def main():
     if not os.path.exists(file_path):
         raise IOError("{0} file doesn't exist".format(file_path))
 
-    found_class = False
-    needs_doc_string = False
-    file_contents = None
-    methods = []
+    current_class = None
+    class_methods = {}
 
     with open(file_path, 'r') as filer:
         file_contents = filer.readlines()
 
     for i in range(len(file_contents)):
         if file_contents[i].startswith('class'):
-            found_class = True
             if '\"\"\"' not in file_contents[i+1]:
-                needs_doc_string = True
-        elif file_contents[i].startswith('    def ') and found_class:
-            if file_contents[i].split('def')[1][1] == '_':  # designated private method
+                current_class = get_class_name_from_line(file_contents[i])
+                print current_class
+                class_methods[current_class] = []
+        elif file_contents[i].startswith('    def '):
+            if file_contents[i].split('def')[1][1] == '_' or current_class is None:
                 continue
-            methods.append(file_contents[i].rstrip())
+            class_methods[current_class].append(file_contents[i].rstrip())
 
-    # remove dev
-    methods = [re.sub('def ', '', x) for x in methods]
+    for key, methods in class_methods.iteritems():
+        # remove def
+        methods = [re.sub('def ', '', x) for x in methods]
 
-    # remove cls and self if exists
-    methods = [re.sub('cls, ', '', x) for x in methods]
-    methods = [re.sub('self, ', '', x) for x in methods]
+        # remove cls and self if exists
+        methods = [re.sub('cls, ', '', x) for x in methods]
+        methods = [re.sub('self, ', '', x) for x in methods]
+        class_methods[key] = methods
 
-    if not needs_doc_string:
-        print "No docstring needed"
+    if len(class_methods) == 0:
+        print "No Docstrings Needing\nExiting."
         return
 
     with open('temp_file', 'w') as filew:
         for line in file_contents:
             if line.startswith('class'):
+                class_name = get_class_name_from_line(line)
                 filew.write(line)
-                filew.write(generate_docstring(methods))
+                filew.write(generate_docstring(class_methods[class_name]))
             else:
                 filew.write(line)
 
@@ -71,6 +73,10 @@ def generate_docstring(methods):
             docstring += '\n'
     docstring += '    \"\"\"\n'
     return docstring
+
+
+def get_class_name_from_line(line):
+    return line.split(' ')[1].split('(')[0]
 
 if __name__ == '__main__':
     main()
