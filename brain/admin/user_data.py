70d9cc4fbdb93a3623b269ce23875d67364baab7
pbraunstein <pbraunstein12@gmail.com>
2017-01-29 10:59:42 -0500

    Docstring

--- brain/admin/user_data.py
+++ brain/admin/user_data.py
@@ -5,6 +5,15 @@ from service import RepExercisesHistoryService, RepExercisesTaxonomyService
 
 
 class UserData(object):
+    """
+    Returns the relevant data for one user
+
+    I N T E R F A C E   G U A R A N T E E D
+    ---------------------------------------
+    get_user_data(cls):
+        -- Returns a dictionary with the nickname, user_id, all rep exercises history and rep exercises taxonomy
+            specific to that user
+    """
     @classmethod
     def get_user_data(cls):
         user_data = {'nickname': cls._get_current_user_nickname(), 'user_id': cls._get_current_user_id(),
