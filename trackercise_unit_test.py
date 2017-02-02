54b699112730f096eb6bd1d2185c6a4c8c740553
pbraunstein <pbraunstein12@gmail.com>
2017-02-01 21:32:34 -0500

    Revert "Touch Trackercise Unittest case"
    This reverts commit 852909f4790480d6db893d0ad93d6c00fc36aee7.

--- trackercise_unit_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from app import create_app
-
-
-class TrackerciseUnittestCase(unittest.TestCase):
-    def setUp(self):
-        super(TrackerciseUnittestCase, self).setUp()
-        self.app = create_app('testing')
