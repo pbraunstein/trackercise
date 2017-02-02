4c6f70175e877966fe8c0c4f0d70fcf3b9de1e6b
pbraunstein <pbraunstein12@gmail.com>
2017-02-01 21:24:23 -0500

    Have default value if constants aren't found

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = os.environ['TRACKERCISE_SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'fake-db'
+    SECRET_KEY = os.environ.get('TRACKERCISE_SECRET_KEY') or 'not-really-that-secret'
 
 
 class ProductionConfig(Config):
