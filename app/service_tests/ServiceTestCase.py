bea89856f99bf67f6a21aed4cd4d468d97781369
pbraunstein <pbraunstein12@gmail.com>
2017-02-08 07:44:16 -0500

    Touch ServiceTestCase

---
+++ app/service_tests/ServiceTestCase.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class ServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        super(ServiceTestCase, self).setUp()
+
+    def tearDown(self):
+        super(ServiceTestCase, self).tearDown()
