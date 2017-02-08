7478c62efb46b91b211cd8fce22ec0ca3165ec63
pbraunstein <pbraunstein12@gmail.com>
2017-02-08 08:18:09 -0500

    test db is created and destroyed by ServiceTestCase

--- app/service_tests/ServiceTestCase.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class ServiceTestCase(unittest.TestCase):
-    def setUp(self):
-        super(ServiceTestCase, self).setUp()
-
-    def tearDown(self):
-        super(ServiceTestCase, self).tearDown()
---
+++ app/service_tests/service_test_case.py
@@ -0,0 +1,19 @@
+import unittest
+
+from app import create_app, db
+
+
+class ServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        super(ServiceTestCase, self).setUp()
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.session.commit()
+        db.create_all()
+
+    def tearDown(self):
+        super(ServiceTestCase, self).tearDown()
+        db.session.commit()
+        db.drop_all()
+        self.app_context.pop()
