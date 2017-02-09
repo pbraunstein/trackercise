2fe3431edd83839436ae3f1e89008dcc0b39da85
pbraunstein <pbraunstein12@gmail.com>
2017-02-09 17:52:28 -0500

    Touch users_history test file

---
+++ app/service_tests/rep_exercises_history_service_tests.py
@@ -0,0 +1,6 @@
+from app.models import RepExercisesHistory
+from app.service import RepExercisesHistoryService
+from app.service_tests.service_test_case import ServiceTestCase
+
+class RepExercisesHistoryTests(ServiceTestCase):
+    pass
