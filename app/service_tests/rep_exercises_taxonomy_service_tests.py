676ef83b82ab540ca517017f227ef86615eac2e1
pbraunstein <pbraunstein12@gmail.com>
2017-02-10 07:24:41 -0500

    Touch test file

---
+++ app/service_tests/rep_exercises_taxonomy_service_tests.py
@@ -0,0 +1,6 @@
+from app.models import RepExercisesTaxonomy
+from app.service import RepExercisesTaxonomyService
+from app.service_tests.service_test_case import ServiceTestCase
+
+class RepExercisesTaxonomyTests(ServiceTestCase):
+    pass
