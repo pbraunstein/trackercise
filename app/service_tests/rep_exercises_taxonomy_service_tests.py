bcc5fa65bcc112a60c50192ee75016491d4c5e32
pbraunstein <pbraunstein12@gmail.com>
2017-02-10 07:45:22 -0500

    Finish rep taxonomies service tests

--- app/service_tests/rep_exercises_taxonomy_service_tests.py
+++ app/service_tests/rep_exercises_taxonomy_service_tests.py
@@ -103,6 +103,58 @@ class RepExercisesTaxonomyTests(ServiceTestCase):
 
         self.assertListEqual(sorted(actual_results, key=self._sort_key), sorted(expected_results, key=self._sort_key))
 
+    # get_list_of_taxonomies_by_exercise_ids #
+    def test_get_list_of_taxonomies_by_exercise_ids(self):
+        entry_1 = RepExercisesTaxonomy(
+            name='test_rows',
+            is_back=True,
+            is_chest=False,
+            is_shoulders=True,
+            is_biceps=True,
+            is_triceps=False,
+            is_legs=False,
+            is_core=True,
+            is_balance=False,
+            is_cardio=False,
+            is_weight_per_hand=True
+        )
+        entry_2 = RepExercisesTaxonomy(
+            name='test_press',
+            is_back=False,
+            is_chest=True,
+            is_shoulders=True,
+            is_biceps=False,
+            is_triceps=True,
+            is_legs=False,
+            is_core=True,
+            is_balance=False,
+            is_cardio=False,
+            is_weight_per_hand=True
+        )
+        entry_3 = RepExercisesTaxonomy(
+            name='test_shrugs_bosu',
+            is_back=True,
+            is_chest=False,
+            is_shoulders=True,
+            is_biceps=False,
+            is_triceps=False,
+            is_legs=False,
+            is_core=True,
+            is_balance=True,
+            is_cardio=False,
+            is_weight_per_hand=True
+        )
+
+        RepExercisesTaxonomyService.add_entry_to_db(entry_1)
+        RepExercisesTaxonomyService.add_entry_to_db(entry_2)
+        RepExercisesTaxonomyService.add_entry_to_db(entry_3)
+
+        expected_results = [entry_1, entry_3]
+        actual_results = RepExercisesTaxonomyService.get_list_of_taxonomies_by_exercise_ids([1, 3])
+
+        # no guarantee of ordering made
+        self.assertListEqual(sorted(actual_results, key=self._sort_key), sorted(expected_results, key=self._sort_key))
+
     @staticmethod
     def _sort_key(x):
         return x.name
