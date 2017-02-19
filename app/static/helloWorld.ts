b7379fbc20bb6a3ab6697bdb420212c1c2be3b61
pbraunstein <pbraunstein12@gmail.com>
2017-02-19 14:49:00 -0500

    Hellow world typescript for angular2

---
+++ app/static/helloWorld.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+@Component({
+    selector: 'i-am',
+    template: '<h2> I am {{ status }} </h2>'
+})
+class IAmAliveComponent {
+    status: string;
+
+    constructor() {
+        this.status = 'Alive';
+    }
+}
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [IAmAliveComponent],
+    bootstrap: [IAmAliveComponent]
+})
+export class PhilTestModule {}
+
+platformBrowserDynamic().bootstrapModule(PhilTestModule);
