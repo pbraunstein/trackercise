656a4087771a95dbb71aec6d421ccd8c113c5d7f
pbraunstein <pbraunstein12@gmail.com>
2017-02-25 09:43:05 -0500

    Remove pre-git parts of the script (these must be done manually for the time being)

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,29 +1,16 @@
 #!/bin/sh
 
 main () {
-    echo "U P D A T I N G  O S . . ."
-    update_os
     echo "I N S T A L L I N G  N E C E S S A R Y  P A C K A G E S . . ."
     install_necessary_packages
-    echo "I N S T A L L I N G  T R A C K E R C I Z E . . ."
-    install_trackercise
-}
-
-update_os () {
-    sudo yum update;
 }
 
 install_necessary_packages() {
-    sudo yum install git-all
     sudo yum install libffi-devel
     sudo yum install nginx
     sudo yum install gcc
     sudo yum install postgresql95-server.x86_64
     sudo yum install postgresql95-devel.x86_64
-}
-
-install_trackercise() {
-    git clone https://github.com/pbraunstein/trackercise.git
 }
 
 main
