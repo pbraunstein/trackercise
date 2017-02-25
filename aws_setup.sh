3e587714428f6e18888fbc881e32dc49cd74088f
pbraunstein <pbraunstein12@gmail.com>
2017-02-25 09:45:00 -0500

    nvm and node install to script

--- aws_setup.sh
+++ aws_setup.sh
@@ -3,6 +3,7 @@
 main () {
     echo "I N S T A L L I N G  N E C E S S A R Y  P A C K A G E S . . ."
     install_necessary_packages
+    echo "I N S T A L L I N G  N V M . . ."
 }
 
 install_necessary_packages() {
@@ -11,6 +12,14 @@ install_necessary_packages() {
     sudo yum install gcc
     sudo yum install postgresql95-server.x86_64
     sudo yum install postgresql95-devel.x86_64
+}
+
+install_nvm() {
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+}
+
+install_node() {
+    nvm install 6.9.5
 }
 
 main
