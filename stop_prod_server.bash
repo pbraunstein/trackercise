b7ad76a8b985025f6b53cedf715bd4b955a5e992
pbraunstein <pbraunstein12@gmail.com>
2017-02-25 20:52:24 -0500

    Better nginx stop

--- stop_prod_server.bash
+++ stop_prod_server.bash
@@ -4,7 +4,7 @@ main () {
     echo "S T O P P I N G  G U N I C O R N . . ."
     kill $(ps aux | grep '[g]unicorn' | awk '{print $2}')
     echo "S T O P P I N G  N G I N X . . ."
-    kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
+    sudo /etc/init.d/nginx stop
 }
 
 main
