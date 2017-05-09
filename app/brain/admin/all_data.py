3a60e747b52c3deb3a586e81a253ddf843bb26c6
pbraunstein <pbraunstein12@gmail.com>
2017-05-08 21:02:26 -0400

    Unit test around REST

--- app/brain/admin/all_data.py
+++ app/brain/admin/all_data.py
@@ -19,33 +19,9 @@ class AllData(object):
         all_results = {}
         user_results = UsersService.get_list_of_all_users()
         taxonomy_results = RepExercisesTaxonomyService.get_list_of_all_exercises()
-        taxonomy_results = [cls._prepare_taxonomy_entry(x) for x in taxonomy_results]
         history_results = RepExercisesHistoryService.get_list_of_all_history()
         history_results = [prepare_history_entry(x) for x in history_results]
         all_results[USERS] = user_results
         all_results[TAXONOMY] = taxonomy_results
         all_results[HISTORY] = history_results
         return all_results
-
-
-    @staticmethod
-    def _prepare_taxonomy_entry(entry):
-        """
-        Turns boolean True or False to YES or NO for RepExercisesTaxonomy entry
-        """
-
-        def debooleanize(value):
-            return 'YES' if value else 'NO'
-
-        entry.is_back = debooleanize(entry.is_back)
-        entry.is_chest = debooleanize(entry.is_chest)
-        entry.is_shoulders = debooleanize(entry.is_shoulders)
-        entry.is_biceps = debooleanize(entry.is_biceps)
-        entry.is_triceps = debooleanize(entry.is_triceps)
-        entry.is_legs = debooleanize(entry.is_legs)
-        entry.is_core = debooleanize(entry.is_core)
-        entry.is_balance = debooleanize(entry.is_balance)
-        entry.is_cardio = debooleanize(entry.is_cardio)
-        entry.is_weight_per_hand = debooleanize(entry.is_weight_per_hand)
-
-        return entry
