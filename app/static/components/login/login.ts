676aec463ccbc71f1ad0858dc1143ef08e028849
pbraunstein <pbraunstein12@gmail.com>
2017-05-10 08:42:05 -0400

    Basic login form for the frontend. It doesn't do anything yet.

--- app/static/components/login/login.ts
+++ app/static/components/login/login.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector:'login',
-    template: '<h1>I will login</h1>'
+    templateUrl: '/static/components/login/login.html'
 })
 export class LoginComponent{
 
