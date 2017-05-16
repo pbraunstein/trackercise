c52298c1151547b9feb57d5dcf6679c65d2cc2cf
pbraunstein <pbraunstein12@gmail.com>
2017-05-16 09:03:32 -0400

    Request made to endpoint successfully

--- app/static/components/addhistory/addhistory.ts
+++ app/static/components/addhistory/addhistory.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {Observable} from "rxjs";
-import {Http} from "@angular/http";
+import {Http, URLSearchParams} from "@angular/http";
 @Component({
     selector: 'add-history',
     templateUrl: '/static/components/addhistory/addhistory.html'
@@ -26,5 +26,18 @@ export class AddHistoryComponent {
 
     onSubmit(value: any){
         console.log(value);
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('history_exercise_id', value.history_exercise_id);
+        params.set('history_sets', value.history_sets);
+        params.set('history_reps', value.history_reps);
+        params.set('history_weight', value.history_weight);
+        params.set('history_date', value.history_date);
+        this.endpoint_add_history = this.http.get('/add-rep-history', {
+            search: params
+        });
+        this.endpoint_add_history.subscribe(
+            data => console.log(data),
+            err => console.log(err)
+        )
     }
 }
