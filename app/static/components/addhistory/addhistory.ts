373d5a3ba3771c69db16a286ef73c525f87ee97d
pbraunstein <pbraunstein12@gmail.com>
2017-05-16 08:26:59 -0400

    Remove unused jquery var

--- app/static/components/addhistory/addhistory.ts
+++ app/static/components/addhistory/addhistory.ts
@@ -1,7 +1,6 @@
 import {Component} from "@angular/core";
 import {Observable} from "rxjs";
 import {Http} from "@angular/http";
-declare const $:JQueryStatic;
 
 @Component({
     selector: 'add-history',
