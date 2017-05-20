a4578430ac622a31811cd65c88492c8e91071603
pbraunstein <pbraunstein12@gmail.com>
2017-05-20 18:25:59 -0400

    Remove unused space below bottom navbar

--- app/static/components/application/application.ts
+++ app/static/components/application/application.ts
@@ -1,7 +1,10 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'application-component',
-    templateUrl: '/static/components/application/application.html'
+    templateUrl: '/static/components/application/application.html',
+    styles: [
+        `#navbar_botom {margin-bottom: 0;}`
+    ]
 })
 export class ApplicationComponent {
 }
