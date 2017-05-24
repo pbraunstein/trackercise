c5e266fae5e53e7239cd1feff79e3a4c8980dfd3
pbraunstein <pbraunstein12@gmail.com>
2017-05-23 20:45:53 -0400

    Add login and logout buttons

--- app/static/routing.ts
+++ app/static/routing.ts
@@ -2,10 +2,15 @@ import {Routes, RouterModule} from "@angular/router";
 import {CurrentUserComponent} from "./components/currentuser/currentuser";
 import {HistoryByTaxonomyComponent} from "./components/historybytaxonomy/historybytaxonomy";
 import {IntroductionComponent} from "./components/introduction/introduction";
+import {LoginComponent} from "./components/login/login";
+import {LogoutComponent} from "./components/logout/logout";
+
 const routes: Routes = [
     {path: '', component: IntroductionComponent},
     {path: 'history-by-taxonomy', component: HistoryByTaxonomyComponent},
-    {path: 'settings', component: CurrentUserComponent}
+    {path: 'settings', component: CurrentUserComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'logout', component: LogoutComponent}
 ];
 
 export const routing = RouterModule.forRoot(routes);
