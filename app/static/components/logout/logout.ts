277ac3e083e07234336aad80dcd2dd2015ea76c5
pbraunstein <pbraunstein12@gmail.com>
2017-05-24 09:10:52 -0400

    Redirects for login and logout

--- app/static/components/logout/logout.ts
+++ app/static/components/logout/logout.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {Observable} from "rxjs";
 import {Http} from "@angular/http";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'logout',
@@ -8,14 +9,19 @@ import {Http} from "@angular/http";
 })
 export class LogoutComponent {
     private endpoint: Observable<any>;
+    private router: Router;
 
-    constructor(private http: Http) {
+    constructor(private http: Http, router: Router) {
         this.endpoint = http.post('/logout', '');
+        this.router = router;
     }
 
     onSubmit() {
         this.endpoint.subscribe(
-            data => console.log(data),
+            data => {
+                console.log(data);
+                this.router.navigate(['login'])
+            },
             err => console.log(err)
         )
     }
