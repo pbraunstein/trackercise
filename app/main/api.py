c6b720bc22f72f6c5c111d3710952e8e93b9141f
pbraunstein <pbraunstein12@gmail.com>
2017-05-27 14:16:22 -0400

    Only posts to the endpoint allowed

--- app/main/api.py
+++ app/main/api.py
@@ -90,7 +90,7 @@ def add_rep_history():
     return dumps({'status': 'good'}), 200
 
 
-@main.route('/get-valid-id-exercise-pairs', methods=['POST', 'GET'])
+@main.route('/get-valid-id-exercise-pairs', methods=['POST'])
 def get_valid_id_exercise_pairs():
     return dumps({
         'pairs': RepExercisesManagement.get_valid_id_exercise_pairs()
