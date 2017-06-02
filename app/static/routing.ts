a0c2bed11020352b0d0db6cdc96c8709f8e5136f
pbraunstein <pbraunstein12@gmail.com>
2017-06-01 21:29:38 -0400

    Put new tabs on the map

--- app/static/routing.ts
+++ app/static/routing.ts
@@ -4,10 +4,14 @@ import {HistoryByTaxonomyComponent} from "./components/historybytaxonomy/history
 import {IntroductionComponent} from "./components/introduction/introduction";
 import {LoginComponent} from "./components/login/login";
 import {LoginGuard} from "./loginguard";
+import {AddHistoryComponent} from "./components/addhistory/addhistory";
+import {AddTaxonomyComponent} from "./components/addtaxonomy/addtaxonomy";
 
 const routes: Routes = [
     {path: '', component: IntroductionComponent},
     {path: 'history-by-taxonomy', component: HistoryByTaxonomyComponent, canActivate:[LoginGuard]},
+    {path: 'add-history-entry', component: AddHistoryComponent, canActivate:[LoginGuard]},
+    {path: 'add-taxonomy-entry', component: AddTaxonomyComponent, canActivate:[LoginGuard]},
     {path: 'settings', component: CurrentUserComponent, canActivate:[LoginGuard]},
     {path: 'login', component: LoginComponent},
 ];
