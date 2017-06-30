759bbd56d70d5e9ff07ca920e1c46e52509469b0
pbraunstein <pbraunstein12@gmail.com>
2017-06-30 08:00:31 -0400

    Is authenticated had the wrong value

--- app/brain/user_management/tests/account_updater_tests.py
+++ app/brain/user_management/tests/account_updater_tests.py
@@ -15,6 +15,6 @@ class AccountUpdateTests(unittest.TestCase):
     @patch('app.brain.user_management.account_updater.current_user')
     @patch('app.brain.user_management.account_updater.UsersService.change_password')
     def test_change_password_user_not_authenticated(self, change_password_mock, current_user_mock):
-        type(current_user_mock.return_value).is_authenticated = True
+        type(current_user_mock.return_value).is_authenticated = False
         result = AccountUpdater.change_password('old_password', 'new_password', 'new_password')
         self.assertEqual(result, ChangePasswordResult.CURRENT_PASSWORD_INCORRECT)
