cd2b8f51fafef18e6a648f1458a51b6e9ea212f8
pbraunstein <pbraunstein12@gmail.com>
2017-07-23 18:09:07 -0400

    Request to history-by-date returns data

--- app/static/components/historybydate/historybydate.ts
+++ app/static/components/historybydate/historybydate.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {CSRFService} from "../../services/csrfservice";
-import {Http} from "@angular/http";
+import {Http, Headers} from "@angular/http";
 import {RepHistory} from "../../models/rephistory";
 import {Observable} from "rxjs";
 @Component({
@@ -10,6 +10,7 @@ import {Observable} from "rxjs";
 export class HistoryByDateComponent {
 
     private endpoint_exercise_pairs: Observable<any>;
+    private endpoint_history_by_date: Observable<any>;
     private pairs: Map<string, string>;
     private exerciseHistory: Array<RepHistory>;
 
@@ -28,7 +29,7 @@ export class HistoryByDateComponent {
     }
 
     private initPairs(jsonObject: any): Map<string, string> {
-        let pairs:Map<string, string> = new Map();
+        let pairs: Map<string, string> = new Map();
 
         for (let aPair of jsonObject) {
             pairs.set(aPair[0], aPair[1]);
@@ -36,6 +37,24 @@ export class HistoryByDateComponent {
 
         console.log(pairs);
         return pairs;
+    }
+
+    onSubmit(form: any) {
+        let value: any = form.value;
+        let headers: Headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('X-CSRFTOKEN', this.csrfService.getToken());
+        let data: any = {};
+        data.exercise_date = value.history_date;
+        this.endpoint_history_by_date = this.http.post(
+            '/history-by-date',
+            JSON.stringify(data),
+            {headers: headers}
+        );
+        this.endpoint_history_by_date.subscribe(
+            data => console.log(data),
+            err => console.log(err)
+        )
     }
 
 }
