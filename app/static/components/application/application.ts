5ca57dc3b99394ec60e0e4e9a752bd42157ce1a9
pbraunstein <pbraunstein12@gmail.com>
2017-08-06 11:08:08 -0400

    Add padding for bottom navbar so it doesn't overlap content

--- app/static/components/application/application.ts
+++ app/static/components/application/application.ts
@@ -5,7 +5,7 @@ import {LogoutService} from "../../services/logoutservice";
     templateUrl: '/static/components/application/application.html',
     styles: [
         `#navbar_botom {margin-bottom: 0; border-radius: 0; position: fixed; bottom: 0; left: 0; right: 0;}`,
-        `#buffer {height: 70px}`
+        `.buffer {height: 70px}`
     ]
 })
 export class ApplicationComponent {
