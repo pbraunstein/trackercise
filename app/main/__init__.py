640458a74d62f06f81bd9bcabe1d5340b9632fb3
pbraunstein <pbraunstein12@gmail.com>
2017-08-09 21:29:05 -0400

    Final flake8 errors

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 main_blueprint = Blueprint('main_blueprint', __name__)
 
-from app.main.api import general_api
-from app.main.api import rep_exercises_api
-from app.main.api import time_exercises_api
-from app.main.api import users_api
+from app.main.api import general_api  # noqa
+from app.main.api import rep_exercises_api  # noqa
+from app.main.api import time_exercises_api  # noqa
+from app.main.api import users_api  # noqa
