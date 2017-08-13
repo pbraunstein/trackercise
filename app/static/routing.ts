d3ea9958f539e4df5e72a4b194f4737c32dd173c
pbraunstein <pbraunstein12@gmail.com>
2017-08-13 08:23:17 -0400

    Implement time history by date component

--- app/static/routing.ts
+++ app/static/routing.ts
@@ -3,14 +3,14 @@ import {CurrentUserComponent} from "./components/currentuser/currentuser";
 import {IntroductionComponent} from "./components/introduction/introduction";
 import {LoginComponent} from "./components/login/login";
 import {LoginGuard} from "./loginguard";
-import {RepHistoryByDateComponent} from "./components/rephistorybydate/rephistorybydate";
 import {AddEntriesComponent} from "./components/addentries/addentries";
 import {HistoryByTaxonomyComponent} from "./components/historybytaxonomy/historybytaxonomy";
+import {HistoryByDateComponent} from "./components/historybydate/historybydate";
 
 const routes: Routes = [
     {path: '', component: IntroductionComponent},
     {path: 'history-by-taxonomy', component: HistoryByTaxonomyComponent, canActivate:[LoginGuard]},
-    {path: 'rep-history-by-date', component: RepHistoryByDateComponent, canActivate:[LoginGuard]},
+    {path: 'history-by-date', component: HistoryByDateComponent, canActivate:[LoginGuard]},
     {path: 'add-entries', component: AddEntriesComponent, canActivate:[LoginGuard]},
     {path: 'settings', component: CurrentUserComponent, canActivate:[LoginGuard]},
     {path: 'login', component: LoginComponent},
