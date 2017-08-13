ca249bc4b1a7e82486727f3049c5579c7daa1d1d
pbraunstein <pbraunstein12@gmail.com>
2017-08-13 11:03:16 -0400

    Reorder import in main.ts

--- app/static/main.ts
+++ app/static/main.ts
@@ -1,36 +1,38 @@
-import 'zone.js';
-import 'reflect-metadata';
-import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import 'reflect-metadata';
+import 'zone.js';
+
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {HttpModule} from "@angular/http";
-import {AllDataComponent} from "./components/alldata/alldata";
-import {LoginComponent} from "./components/login/login";
-import {FormsModule} from "@angular/forms";
-import {CurrentUserComponent} from "./components/currentuser/currentuser";
-import {RegisterComponent} from "./components/register/register";
-import {UserDataComponent} from "./components/userdata/userdata";
-import {AddRepTaxonomyComponent} from "./components/addreptaxonomy/addreptaxonomy";
+
+import {AddEntriesComponent} from "./components/addentries/addentries";
 import {AddRepHistoryComponent} from "./components/addrephistory/addrephistory";
-import {RepHistoryByTaxonomyComponent} from "./components/rephistorybytaxonomy/rephistorybytaxonomy";
+import {AddRepTaxonomyComponent} from "./components/addreptaxonomy/addreptaxonomy";
+import {AddTimeHistoryComponent} from "./components/addtimehistory/addtimehistory";
+import {AddTimeTaxonomyComponent} from "./components/addtimetaxonomy/addtimetaxonomy";
+import {AllDataComponent} from "./components/alldata/alldata";
 import {ApplicationComponent} from "./components/application/application";
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
-import {routing} from './routing';
+import {ChangePasswordComponent} from "./components/changepassword/changepassword";
+import {CurrentUserComponent} from "./components/currentuser/currentuser";
+import {HistoryByDateComponent} from "./components/historybydate/historybydate";
+import {HistoryByTaxonomyComponent} from "./components/historybytaxonomy/historybytaxonomy";
 import {IntroductionComponent} from "./components/introduction/introduction";
+import {LoginComponent} from "./components/login/login";
+import {RegisterComponent} from "./components/register/register";
+import {RepHistoryByDateComponent} from "./components/rephistorybydate/rephistorybydate";
+import {RepHistoryByTaxonomyComponent} from "./components/rephistorybytaxonomy/rephistorybytaxonomy";
+import {TimeHistoryByDateComponent} from "./components/timehistorybydate/timehistorybydate";
+import {TimeHistoryByTaxonomyComponent} from "./components/timehistorybytaxonomy/timehistorybytaxonomy";
+import {UserDataComponent} from "./components/userdata/userdata";
 import {LoginGuard} from "./loginguard";
+import {routing} from './routing';
 import {CSRFService} from "./services/csrfservice";
 import {LogoutService} from "./services/logoutservice";
-import {ChangePasswordComponent} from "./components/changepassword/changepassword";
-import {RepHistoryByDateComponent} from "./components/rephistorybydate/rephistorybydate";
-import {AddEntriesComponent} from "./components/addentries/addentries";
-import {AddTimeTaxonomyComponent} from "./components/addtimetaxonomy/addtimetaxonomy";
-import {AddTimeHistoryComponent} from "./components/addtimehistory/addtimehistory";
-import {TimeHistoryByTaxonomyComponent} from "./components/timehistorybytaxonomy/timehistorybytaxonomy";
-import {HistoryByTaxonomyComponent} from "./components/historybytaxonomy/historybytaxonomy";
-import {HistoryByDateComponent} from "./components/historybydate/historybydate";
-import {TimeHistoryByDateComponent} from "./components/timehistorybydate/timehistorybydate";
 
 @NgModule({
     imports: [BrowserModule, HttpModule, FormsModule, routing],
