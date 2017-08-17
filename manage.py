6f647fb98bf087bc4a2a2e5e434665dd364bba7c
pbraunstein <pbraunstein12@gmail.com>
2017-08-17 08:10:13 -0400

    DRY up code -- a single import function

--- manage.py
+++ manage.py
@@ -66,6 +66,65 @@ IMPORT_TIME_HISTORY_FILE_PATH = os.path.join(
 )
 
 
+def _generate_user_from_row(row):
+    return Users(
+        email=row[0],
+        nickname=row[1],
+        password=row[2]
+    )
+
+
+def _generate_rep_taxonomy_from_row(row):
+    return RepExercisesTaxonomy(
+        name=row[0].upper(),
+        is_back=_string_to_bool(row[1]),
+        is_chest=_string_to_bool(row[2]),
+        is_shoulders=_string_to_bool(row[3]),
+        is_biceps=_string_to_bool(row[4]),
+        is_triceps=_string_to_bool(row[5]),
+        is_legs=_string_to_bool(row[6]),
+        is_core=_string_to_bool(row[7]),
+        is_balance=_string_to_bool(row[8]),
+        is_cardio=_string_to_bool(row[9]),
+        is_weight_per_hand=_string_to_bool(row[10])
+    )
+
+
+def _generate_rep_history_from_row(row):
+    return RepExercisesHistory(
+        user_id=int(row[0]),
+        exercise_id=int(row[1]),
+        sets=int(row[2]),
+        reps=int(row[3]),
+        weight=float(row[4]),
+        date=row[5]
+    )
+
+
+def _generate_time_taxonomy_from_row(row):
+    return TimeExercisesTaxonomy(
+        row[0].upper()
+    )
+
+
+def _generate_time_history_from_row(row):
+    return TimeExercisesHistory(
+        user_id=int(row[0]),
+        exercise_id=int(row[1]),
+        distance=float(row[2]),
+        duration=int(row[3]),
+        exercise_date=row[4]
+    )
+
+IMPORT_FILE_PATHS_FACTORY_METHOD_MAP = {
+    IMPORT_USERS_FILE_PATH: _generate_user_from_row,
+    IMPORT_REP_TAXONOMY_FILE_PATH: _generate_rep_taxonomy_from_row,
+    IMPORT_REP_HISTORY_FILE_PATH: _generate_rep_history_from_row,
+    IMPORT_TIME_TAXONOMY_FILE_PATH: _generate_time_taxonomy_from_row,
+    IMPORT_TIME_HISTORY_FILE_PATH: _generate_time_history_from_row
+}
+
+
 @manager.command
 def backup_data_to_s3():
     """
@@ -88,20 +147,35 @@ def _clean_up_from_export():
 
 @manager.command
 def run_importers():
-    """
-    Import rep taxonomy and rep history
-    """
-    import_users()
-    import_rep_taxonomies()
-    import_time_taxonomies()
-    import_rep_history()
-    import_time_history()
+    import_model(IMPORT_USERS_FILE_PATH, IMPORT_FILE_PATHS_FACTORY_METHOD_MAP[IMPORT_USERS_FILE_PATH])
+    import_model(IMPORT_REP_TAXONOMY_FILE_PATH, IMPORT_FILE_PATHS_FACTORY_METHOD_MAP[IMPORT_REP_TAXONOMY_FILE_PATH])
+    import_model(IMPORT_REP_HISTORY_FILE_PATH, IMPORT_FILE_PATHS_FACTORY_METHOD_MAP[IMPORT_REP_HISTORY_FILE_PATH])
+    import_model(IMPORT_TIME_TAXONOMY_FILE_PATH, IMPORT_FILE_PATHS_FACTORY_METHOD_MAP[IMPORT_TIME_TAXONOMY_FILE_PATH])
+    import_model(IMPORT_TIME_HISTORY_FILE_PATH, IMPORT_FILE_PATHS_FACTORY_METHOD_MAP[IMPORT_TIME_HISTORY_FILE_PATH])
 
 
 @manager.command
 def run_exporters():
     for file_path, model in EXPORT_FILE_PATHS_MODELS_MAP.iteritems():
         export_model(file_path, model)
+
+
+def import_model(file_path, factory_method):
+    entries = []
+    with open(file_path, 'rb') as csvfile:
+        data_reader = reader(csvfile)
+        data_reader.next()  # skip header line
+
+        for row in data_reader:
+            try:
+                row = row[1:]  # remove previous row id
+                entries.append(
+                    factory_method(row)
+                )
+            except:  # messy but effective
+                pass
+    db.session.add_all(entries)
+    db.session.commit()
 
 
 def export_model(file_path, model):
@@ -114,128 +188,6 @@ def export_model(file_path, model):
                 data_writer.writerow(d.get_attribute_list())
             except:  # messy but it works
                 pass
-
-
-@manager.command
-def import_users():
-    entries = []
-    with open(IMPORT_USERS_FILE_PATH, 'rb') as csvfile:
-        users_reader = reader(csvfile)
-        users_reader.next()  # skip the header line
-        for row in users_reader:
-            try:
-                row = row[1:]  # remove previous row id
-                entries.append(
-                    Users(
-                        email=row[0],
-                        nickname=row[1],
-                        password=row[2]
-                    )
-                )
-            except:  # messy but effective
-                pass
-    db.session.add_all(entries)
-    db.session.commit()
-
-
-@manager.command
-def import_rep_taxonomies():
-    """
-    Imports sample rep taxonomy data into the rep_exercise_taxonomy table
-    """
-    entries = []
-    with open(IMPORT_REP_TAXONOMY_FILE_PATH, 'rb') as csvfile:
-        taxonomy_reader = reader(csvfile)
-        taxonomy_reader.next()  # skip header line
-        for row in taxonomy_reader:
-            try:
-                row = row[1:]  # remove the previous row id that was in the table
-                entries.append(RepExercisesTaxonomy(
-                    row[0].upper(),
-                    _string_to_bool(row[1]),
-                    _string_to_bool(row[2]),
-                    _string_to_bool(row[3]),
-                    _string_to_bool(row[4]),
-                    _string_to_bool(row[5]),
-                    _string_to_bool(row[7]),
-                    _string_to_bool(row[6]),
-                    _string_to_bool(row[10]),
-                    _string_to_bool(row[9]),
-                    _string_to_bool(row[8]),
-                ))
-            except:  # messy but effective
-                pass
-    db.session.add_all(entries)
-    db.session.commit()
-
-
-@manager.command
-def import_time_taxonomies():
-    entries = []
-    with open(IMPORT_TIME_TAXONOMY_FILE_PATH, 'rb') as csvfile:
-        taxonomy_reader = reader(csvfile)
-        taxonomy_reader.next()  # skip header line
-        for row in taxonomy_reader:
-            try:
-                row = row[1:]  # remove the previous row id
-                entries.append(
-                    TimeExercisesTaxonomy(
-                        row[0].upper()
-                    )
-                )
-            except:  # messy but effective
-                pass
-    db.session.add_all(entries)
-    db.session.commit()
-
-
-@manager.command
-def import_rep_history():
-    """
-    Imports the rep exercise history sample data into the rep_exercises_history db table
-    """
-    entries = []
-    with open(IMPORT_REP_HISTORY_FILE_PATH, 'rb') as csvfile:
-        history_reader = reader(csvfile)
-        history_reader.next()  # skip header line
-        for row in history_reader:
-            row = row[1:]
-            try:
-                entries.append(
-                    RepExercisesHistory(
-                        user_id=int(row[0]),
-                        exercise_id=int(row[1]),
-                        sets=int(row[2]),
-                        reps=int(row[3]),
-                        weight=float(row[4]),
-                        date=row[5]
-                    )
-                )
-            except:  # messy but effective
-                pass
-    db.session.add_all(entries)
-    db.session.commit()
-
-
-@manager.command
-def export_rep_history():
-    """
-    Exports the rep exercise history from the db into a csv file
-    """
-    history = RepExercisesHistory.query.all()
-    with open(EXPORT_REP_HISTORY_FILE_PATH, 'w') as csvfile:
-        history_writer = writer(csvfile)
-        history_writer.writerow(RepExercisesHistory.get_attribute_header_list())
-        for h in history:
-            try:
-                history_writer.writerow(h.get_attribute_list())
-            except:  # messy but effective
-                pass
-
-
-@manager.command
-def import_time_history():
-    pass
 
 
 def _string_to_bool(string):
